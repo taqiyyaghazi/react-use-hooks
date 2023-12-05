fcc6f8d8cf5f0e5ffc78b8a8639de0c783f1f1ea
taqiyyaghazi <taqiyyaghazi@gmail.com>
2023-12-05 10:21:52 +0700

    chore: update commitlint rules

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,16 +1,38 @@
-// build: development changes - related to the build system
-// ci: development changes - related to the continuous integration and deployment system
-// docs: documentation changes
-// feat: production changes - new feature
-// fix: production changes - bug fix
-// perf: production changes - improve performance
-// refactor: development changes - neither adds a feature nor fixes a bug
-// style: development changes - pure styling changes
-// test: development changes - affecting any tests
+// build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+// ci: Changes to CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+// chore: Changes which doesn't change source code or tests e.g. changes to the build process, auxiliary tools, libraries
+// docs: Documentation only changes
+// feat: A new feature
+// fix: A bug fix
+// perf: A code change that improves performance
+// refactor: A code change that neither fixes a bug nor adds a feature
+// revert: Revert something
+// style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+// test: Adding missing tests or correcting existing tests
 
 // commit conventions
 // type(scope?): subject
 // body?
 // footer?
 
-module.exports = { extends: ['@commitlint/config-conventional'] }
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['chore', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'style', 'test', 'revert'],
+    ],
+  },
+}
