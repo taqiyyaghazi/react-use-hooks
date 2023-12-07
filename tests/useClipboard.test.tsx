7f870f9a80815cb5e043c2e6d9ec7a1fb494acb6
taqiyyaghazi <taqiyyaghazi@gmail.com>
2023-12-07 18:24:37 +0700

    test(useClipboard): add test case

--- tests/useClipboard.test.tsx
+++ tests/useClipboard.test.tsx
@@ -1,5 +1,66 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useClipboard, UseClipboardOptions } from '../src/hooks/useClipboard'
+
 describe('useClipboard', () => {
-  it('should to be thruty', () => {
-    expect(true).toBeTruthy()
+  // Mock navigator.clipboard API
+  beforeAll(() => {
+    Object.assign(navigator, {
+      clipboard: {
+        readText: jest.fn(() => Promise.resolve('MockClipboardText')),
+        writeText: jest.fn(),
+      },
+      permissions: {
+        query: jest.fn().mockImplementation(() => Promise.resolve({ state: 'granted' })),
+      },
+    })
+  })
+
+  // Clean up mocks
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  it('should initialize with the correct state', () => {
+    const { result } = renderHook(() => useClipboard())
+
+    expect(result.current.isSupported).toBe(true)
+    expect(result.current.text).toBe('')
+    expect(result.current.copied).toBe(false)
+  })
+
+  it('should update text when copy is called', async () => {
+    const options: UseClipboardOptions = { read: true }
+    const { result, waitForNextUpdate } = renderHook(() => useClipboard(options))
+
+    await act(async () => {
+      await result.current.copy('NewClipboardText')
+      await waitForNextUpdate({ timeout: 1000 })
+    })
+
+    expect(result.current.text).toBe('NewClipboardText')
+  })
+
+  it('should not copy text if clipboard API is not supported', async () => {
+    Object.assign(navigator, { clipboard: undefined })
+    const { result } = renderHook(() => useClipboard())
+
+    await act(async () => {
+      await result.current.copy('NewClipboardText')
+    })
+
+    expect(result.current.text).toBe('')
+    expect(result.current.copied).toBe(false)
+  })
+
+  it('should not read text if read permission is denied', async () => {
+    Object.assign(navigator.permissions, {
+      query: jest.fn().mockImplementation(() => Promise.resolve({ state: 'denied' })),
+    })
+    const options: UseClipboardOptions = { read: true }
+    const { result, waitForNextUpdate } = renderHook(() => useClipboard(options))
+
+    await waitForNextUpdate()
+
+    expect(result.current.text).toBe('')
   })
 })
