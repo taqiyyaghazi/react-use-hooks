4a68e6ee9149fa18784735ba44a25ba64f1c0913
taqiyyaghazi <taqiyyaghazi@gmail.com>
2023-12-08 08:00:16 +0700

    test(useColorMode): add test case

--- tests/useColormode.test.tsx
+++ tests/useColormode.test.tsx
@@ -1,5 +1,36 @@
+import '@testing-library/jest-dom'
+import { act, render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import { BasicColorMode, useColorMode } from '../src/hooks/useColorMode'
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useEffect: jest.fn(),
+}))
+
 describe('useColorMode', () => {
-  it('should true to be truthy', () => {
-    expect(true).toBeTruthy()
+  it('should update the data-theme attribute correctly', async () => {
+    const TestComponent = () => {
+      const { mode, setMode } = useColorMode<BasicColorMode>('light', { selector: 'div' })
+      return (
+        <div>
+          <span data-testid='color-mode'>{mode}</span>
+          <button onClick={() => setMode('dark')}>Toggle Mode</button>
+        </div>
+      )
+    }
+
+    render(<TestComponent />)
+
+    expect(screen.getByTestId('color-mode')).toHaveTextContent('light')
+
+    act(() => {
+      userEvent.click(screen.getByText('Toggle Mode'))
+    })
+
+    await waitFor(() => {
+      expect(screen.getByTestId('color-mode')).toHaveTextContent('dark')
+    })
   })
 })
