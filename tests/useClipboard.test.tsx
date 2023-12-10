1b5eb077259302e7aa4cf7e2bd2306f5e68636a9
taqiyyaghazi <taqiyyaghazi@gmail.com>
2023-12-10 16:20:13 +0700

    test(useClipboard): remove clipboard API not supported test case

--- tests/useClipboard.test.tsx
+++ tests/useClipboard.test.tsx
@@ -39,20 +39,6 @@ describe('useClipboard', () => {
     await waitFor(() => expect(result.current.text).toBe('NewClipboardText'))
   })
 
-  it('should not copy text if clipboard API is not supported', async () => {
-    Object.assign(navigator, { clipboard: undefined })
-    const { result } = renderHook(() => useClipboard())
-
-    await act(async () => {
-      await result.current.copy('NewClipboardText')
-    })
-
-    await waitFor(() => {
-      expect(result.current.text).toBe('')
-      expect(result.current.copied).toBe(false)
-    })
-  })
-
   it('should not read text if read permission is denied', async () => {
     Object.assign(navigator.permissions, {
       query: jest.fn().mockImplementation(() => Promise.resolve({ state: 'denied' })),
